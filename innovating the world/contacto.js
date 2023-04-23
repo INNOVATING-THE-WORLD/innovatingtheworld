6b1801685fad9f6dd3784fbaef2ea596765ea773
INNOVATING-THE-WORLD <131560801+INNOVATING-THE-WORLD@users.noreply.github.com>
2023-04-22 22:41:32 -0500

    Add files via upload

---
+++ innovating the world/contacto.js
@@ -0,0 +1,6 @@
+function enviado(){
+    var nombr=document.getElementById("nombre").value;
+    window.alert("HOLA "+nombr+" SE HA ENVIADO TUS DATOS CON EXITO!!! DURANTE EL TRANSCURSO DEL DIA TE LLAMAMOS");
+
+    
+}
